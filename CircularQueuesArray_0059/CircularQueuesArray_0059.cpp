97c1849a418e8daaaa037590f72b78f9ce0b30f9
ezraabrari <ezraabrari13@gmail.com>
2024-05-31 14:28:43 +0700

    Membuat Front dan Rear di luar index array terlebih dahulu

--- CircularQueuesArray_0059/CircularQueuesArray_0059.cpp
+++ CircularQueuesArray_0059/CircularQueuesArray_0059.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 using namespace std;
 
-class queues {
+class Queues {
 	int FRONT, REAR, max = 5;
 	int queue_array[5];
+
+public:
+	Queues() {
+		FRONT = -1;
+		REAR = -1;
+	}
 };
